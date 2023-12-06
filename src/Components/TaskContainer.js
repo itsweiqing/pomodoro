0bc6773d6db21abb0d9ef705f5f56ee76d5f3352
Veronica Yung <71048282+veronicaYung@users.noreply.github.com>
2023-12-06 09:35:15 +0000

    task container created and sort of styled

--- src/Components/TaskContainer.js
+++ src/Components/TaskContainer.js
@@ -3,6 +3,7 @@ import Button from './Button';
 import addIcon from '../Images/add_icon.png';
 import TaskList from '../Models/taskList';
 import Task from '../Models/task';
+import playIcon from '../Images/play_icon.png'; 
 
 
 const TaskContainer = () => {
@@ -25,22 +26,24 @@ const TaskContainer = () => {
   };
 
   return (
-    <div>
-      <h2>My Tasks</h2>
-      <Button icon={addIcon} altText="Add Task" onClick={handleAddClick} />
-      {/* if is adding is true, show the input field and save button */}
-      {isAdding && (
-        <div>
-        {/* Save input as newTask, always get the most up to date text field with on change  */}
-          <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
-          <button onClick={handleSaveClick}>Save</button>
+    <div className = "TaskList-div">
+        <div className="Task-header">
+            <h2 className="Task-title">My Tasks</h2>
+            <Button icon={addIcon} className="App-button" altText="Add Task" onClick={handleAddClick} />
         </div>
-      )}
-      <ul className = "Task-list">
-        {taskList.toArray().map((task, index) => (
-          <li key={index} className="Task-list-item">{task.description}</li>
-        ))}
-      </ul>
+        {/* if is adding is true, show the input field and save button */}
+        {isAdding && (
+            <div className="CreateTask-div">
+            {/* Save input as newTask, always get the most up to date text field with on change  */}
+            <input type="text" className="Task-input" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+            <button onClick={handleSaveClick}>Save</button>
+            </div>
+        )}
+        <ul className = "Task-list">
+            {taskList.toArray().map((task, index) => (
+            <li key={index} className="Task-list-item">{task.description}</li>
+            ))}
+        </ul>
     </div>
   );
 };
