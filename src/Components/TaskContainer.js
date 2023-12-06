370b30b9bfa0ea85f369b65eaaa3ddc6fbc9cad0
Veronica Yung <71048282+veronicaYung@users.noreply.github.com>
2023-12-06 08:36:47 +0000

    can now add tasks

--- src/Components/TaskContainer.js
+++ src/Components/TaskContainer.js
@@ -1,8 +1,9 @@
-// TaskContainer.js
 import React, { useState } from 'react';
 import Button from './Button';
 import addIcon from '../Images/add_icon.png';
 import TaskList from '../Models/taskList';
+import Task from '../Models/task';
+
 
 const TaskContainer = () => {
   const [taskList, setTaskList] = useState(new TaskList());
@@ -14,7 +15,11 @@ const TaskContainer = () => {
   };
 
   const handleSaveClick = () => {
-    taskList.insertTask(newTask);
+    // Create a new Task object with the description set to newTask
+    let task = new Task(newTask);
+    // Insert the new task into the existing taskList
+    taskList.insertTask(task);
+    // Reset the newTask state to an empty string
     setNewTask('');
     setIsAdding(false);
   };
@@ -23,15 +28,17 @@ const TaskContainer = () => {
     <div>
       <h2>My Tasks</h2>
       <Button icon={addIcon} altText="Add Task" onClick={handleAddClick} />
+      {/* if is adding is true, show the input field and save button */}
       {isAdding && (
         <div>
+        {/* Save input as newTask, always get the most up to date text field with on change  */}
           <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
           <button onClick={handleSaveClick}>Save</button>
         </div>
       )}
-      <ul>
+      <ul className = "Task-list">
         {taskList.toArray().map((task, index) => (
-        <li key={index}>{task.description}</li>
+          <li key={index} className="Task-list-item">{task.description}</li>
         ))}
       </ul>
     </div>
